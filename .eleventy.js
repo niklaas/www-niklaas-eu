998c31ba46793b89edcfab30fa510ecd70c9cc93
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2021-03-28 21:07:57 +0200

    feat: add robots.txt to disallow search engines

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,17 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.setUseGitIgnore(false)
-  eleventyConfig.addWatchTarget("./src/css")
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addWatchTarget("./src/css");
 
-  eleventyConfig.addPassthroughCopy("./src/images")
-  eleventyConfig.addPassthroughCopy("./src/css")
+  eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy({
+    "./src/robots.disallow.txt": "robots.txt",
+  });
 
   return {
     dir: {
       input: "src",
-      output: "dist"
-    }
-  }
-}
+      output: "dist",
+    },
+  };
+};
