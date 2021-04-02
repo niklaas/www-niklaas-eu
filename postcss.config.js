2d62f18a90f2ded5720d0a5c6e7d6a4ce7ecfa1e
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2021-04-02 16:16:07 +0200

    feat: use tailwind's just-in-time compiler

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,12 @@
-module.exports = postcssConfig => {
-  const production = process.env.NODE_ENV === "production"
+module.exports = (postcssConfig) => {
+  const production = process.env.NODE_ENV === "production";
 
   return {
     plugins: [
-      require('precss'),
-      require('tailwindcss')(),
-      require('autoprefixer'), // use this via cssnano advanced preset
-      production && require('cssnano')({ preset: 'default' }),
-    ]
-  }
-}
+      require("precss"),
+      require("@tailwindcss/jit")(),
+      require("autoprefixer"), // TODO: use this via cssnano advanced preset
+      production && require("cssnano")({ preset: "default" }),
+    ],
+  };
+};
