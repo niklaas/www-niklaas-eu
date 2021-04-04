ab2ec4e958441d23e9254547fd9e4145da5f6991
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2021-04-04 12:36:15 +0200

    feat: provide shortcode for responsive images

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,30 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  const metadata = await Image(src, {
+    outputDir: "./dist/images",
+    urlPath: "/images/",
+    widths: [640, 768, 1024, 1280, 1536],
+  });
+  const imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function (eleventyConfig) {
+  // scss processing
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addWatchTarget("./src/css");
-
-  eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addPassthroughCopy("./src/css");
+
+  // image processing
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+
   eleventyConfig.addPassthroughCopy({
     "./src/robots.disallow.txt": "robots.txt",
   });
