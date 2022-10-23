773cb143a9d650709e748bbfe37b3cc779b4a4bf
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2022-10-23 16:49:00 -0300

    feat: bootstrap minified index.html

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,20 @@
+const htmlmin = require("html-minifier");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    if (this.outputPath?.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        collapseWhitespace: true,
+        minifyCSS: true,
+        removeComments: true,
+        useShortDoctype: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
+
   return {
     dir: {
       input: "src",
